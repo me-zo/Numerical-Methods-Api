493ea3f5f6caa8dc1c4f67663c623b60f9a18bb5
Mohammed Zeidan <48134620+Mezo0099@users.noreply.github.com>
2021-02-05 19:27:58 +0200

    Update 1-Taylor-Series.py

--- 1-Taylor-Series.py
+++ 1-Taylor-Series.py
@@ -20,16 +20,19 @@ F = x**2*sp.exp(0.5*x+1)-x +2
 
 FxArr = [1.1,1.4,1.4]
 
+n = 5 # Number Of Orders
+
+# Applying The Algorithm
 
 if Equation == 1:
     print("  ___________________________________________________\n   Order |   Fx0    | Approx value |  Error"
       "\n  ---------------------------------------------------")
 def Fact (f,valfact) :
-    if valfact!= 0:  return (f/sp.factorial(valfact))*(XAt-X0)**valfact
+    if valfact!= 0 :  return (f/sp.factorial(valfact))*(XAt-X0)**valfact
     else :  return f.subs(x, X0).evalf()/sp.factorial(valfact)
 def Error (val): return ((F.subs(x, XAt).evalf()-val)/F.subs(x, XAt).evalf())*100
 Fx = F; Value = 0
-for i in range(5):
+for i in range(n):
     if Equation == 1:
         Value += Fact(Fx.subs(x, X0).evalf(),i)
         print("    {0:2d}   | {1:.4f} |    {2:.4f}  |  {3:.2f} % ".format(i,float(Fx.subs(x, X0)),float(Value),float(Error(Value))))
@@ -39,4 +42,4 @@ for i in range(5):
         elif i < len(FxArr) :  Value += (FxArr[i]/sp.factorial(i))*(XAt-X0)**i
 if Equation == 0:  print("\nThe value of f({0:.3f} ) at x = {1:.3f} is   {2:.5f}\n".format(float(XAt),float(X0), float(Value)))
 
-# Finally The program prints the solution 
+# Finally if Equation = "0" The program prints the solution ELSE if Equation = "1" it will go through each order while providing the error percentage after each order 
